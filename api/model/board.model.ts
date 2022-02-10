cccb8088aa7def084028d384ae06bfbbbc4f3b12
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2022-02-10 12:25:53 -0800

    chore: integrating mongoose wip

--- api/model/board.model.ts
+++ api/model/board.model.ts
@@ -1,12 +1,20 @@
 import { Schema, Model } from "mongoose";
 
-const kabanSchema: any = new Schema({
+const BoardSchema: any = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
   },
+  card: {
+    type: Array,
+    default: [],
+  },
 });
 
-const kabanModel: any = new Model("kaban", kabanSchema);
+const kabanModel: any = new Model("Kaban", BoardSchema);
 
 export default kabanModel;
