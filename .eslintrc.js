097f3e8e10b7395d70de055826cf8f03f1640d76
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2022-02-11 12:12:32 -0800

    chore:added router types and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,22 @@
 module.exports = {
-  root: true,
-  ignorePatterns: ["next.config.js", "jest.config.js", "node_modules/"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react-hooks"],
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
+    'standard',
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/strict",
+    "plugin:@typescript-eslint/recommended"
   ],
-  settings: {
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version
-      // of React to use
-      version: "detect",
-    },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: [
+    "@typescript-eslint"
+  ],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react-hooks/rules-of-hooks": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-    "@typescript-eslint/ban-types": [
-      "error",
-      {
-        "extendDefaults": true,
-        "types": {
-          "{}": false
-        }
-      }
-    ],
-  "jsx-a11y/anchor-is-valid": "off",
-  "@typescript-eslint/camelcase": "off",
-  "@typescript-eslint/no-empty-function": "off",
-  "@typescript-eslint/no-non-null-assertion": "off",
-  "@typescript-eslint/no-var-requires": "off",
-  "react/display-name": "off",
-},
-};
+  }
+}
